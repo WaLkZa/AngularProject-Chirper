77f1d83fbc9e431dd9b473f90b64b12316cc8fa2
WaLkZa <walkzi@yandex.com>
2018-08-23 15:09:57 +0300

    cosmetics changes

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class NavigationComponent implements OnInit {
 
   constructor(
-    private authService: AuthService,
+    public authService: AuthService,
     private router: Router,
   ) { }
 
